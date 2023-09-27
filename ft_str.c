3b6ff163892c509c3401e85a0d613eaaa611bf0b
ecortes- <edu@IdeapadEdu>
2023-09-27 11:40:16 +0200

    1

--- ft_str.c
+++ ft_str.c
@@ -1,12 +1,21 @@
 #include "libft.h"
 
+int ft_char(int c);
+
 int	ft_str(char *str)
 {
+	size_t	i;
+
 	if (!str)
 	{
 		write(1, "(null)", 6);
 		return (6);
 	}
-	ft_putstr_fd(str, 1);
+	i = 0;
+	while (i < ft_strlen(str))
+	{
+		ft_char(str[i]);
+		i++;
+	}
 	return (ft_strlen(str));
 }
